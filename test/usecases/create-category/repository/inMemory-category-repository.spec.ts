14a1589b3da1c03cb0d494959dc773b43b1ebc4b
Luiz <luizz.ggr@gmail.com>
2022-01-28 16:33:11 -0300

    test: return all categories in the repository

--- test/usecases/create-category/repository/inMemory-category-repository.spec.ts
+++ test/usecases/create-category/repository/inMemory-category-repository.spec.ts
@@ -21,4 +21,24 @@ describe('In memory repository', () => {
     const result = await categoryRepo.findCategoryById('my id');
     expect(result.name).toBe('my name');
   });
+
+  test('should find all categories', async () => {
+    const firstUser = {
+      id: 'my id',
+      name: 'my name',
+      description: 'my description',
+      created_at: new Date(),
+    };
+    const secondUser = {
+      id: 'my id2',
+      name: 'my name2',
+      description: 'my description2',
+      created_at: new Date(),
+    };
+    const categories: ICategoryData[] = [firstUser, secondUser];
+    const categoryRepo = new InMemoryCategoryRepository(categories);
+
+    const returnedCategory = await categoryRepo.findAllCategories();
+    expect(returnedCategory.length).toBe(2);
+  });
 });
