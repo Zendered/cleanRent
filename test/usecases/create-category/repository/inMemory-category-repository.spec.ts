a500d2c30c1c07dc4441f2622a3c55e133d9960c
Luiz <luizz.ggr@gmail.com>
2022-01-28 16:18:56 -0300

    test: add and find users and check its existence

--- test/usecases/create-category/repository/inMemory-category-repository.spec.ts
+++ test/usecases/create-category/repository/inMemory-category-repository.spec.ts
@@ -5,9 +5,20 @@ import { ICategoryRepository } from '@/usecases/ports/category-repository';
 describe('In memory repository', () => {
   const categories: ICategoryData[] = [];
   const categoryRepo: ICategoryRepository = new InMemoryCategoryRepository(categories);
-  // const usecase: CreateCategory = new CreateCategory(repo);
   test('should return null if category is not found', async () => {
     const category = await categoryRepo.findCategoryById('my id');
     expect(category).toBeNull();
+  });
+
+  test('should return a existent category', async () => {
+    const category:ICategoryData = {
+      id: 'my id',
+      name: 'my name',
+      description: 'my description',
+      created_at: new Date(),
+    };
+    await categoryRepo.add(category);
+    const result = await categoryRepo.findCategoryById('my id');
+    expect(result.name).toBe('my name');
   });
 });
