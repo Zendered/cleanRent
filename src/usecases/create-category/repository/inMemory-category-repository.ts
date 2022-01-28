0e9c7f2bc1d1ed32de10b03195c65728457c1c06
Luiz <luizz.ggr@gmail.com>
2022-01-28 16:18:44 -0300

    feat: add and find users and check its existence

--- src/usecases/create-category/repository/inMemory-category-repository.ts
+++ src/usecases/create-category/repository/inMemory-category-repository.ts
@@ -2,17 +2,24 @@ import { ICategoryRepository } from '@/usecases/ports/category-repository';
 import { ICategoryData } from '../category-data';
 
 export class InMemoryCategoryRepository implements ICategoryRepository {
-  private readonly repository:ICategoryData[];
+  private repository:ICategoryData[];
 
   constructor(repository: ICategoryData[]) {
     this.repository = repository;
   }
 
   async add(category: ICategoryData): Promise<void> {
-    throw new Error('Method not implemented.');
+    const exists = await this.exists(category);
+    if (!exists) {
+      this.repository.push(category);
+    }
   }
 
   async findCategoryById(id: string): Promise<ICategoryData> {
+    const category = await this.repository.filter((category) => category.id === id);
+    if (category.length > 0) {
+      return category[0];
+    }
     return null;
   }
 
@@ -21,6 +28,9 @@ export class InMemoryCategoryRepository implements ICategoryRepository {
   }
 
   async exists(category: ICategoryData): Promise<boolean> {
-    throw new Error('Method not implemented.');
+    if (await this.findCategoryById(category.id) === null) {
+      return false;
+    }
+    return true;
   }
 }
