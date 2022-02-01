add50e235ea3003cc2d3130985bc1b59cd224138
Luiz <luizz.ggr@gmail.com>
2022-02-01 14:19:37 -0300

    test: validating if category has invalid data and if its null

--- test/entities/category-validation.spec.ts
+++ test/entities/category-validation.spec.ts
@@ -1,18 +1,34 @@
 import { Category } from '@/entities/category';
+import { ICategoryData } from '@/entities/';
 
-describe('category validation', () => {
-  test('should not accept null strings', () => {
-    const category = null;
+describe('Category validation', () => {
+  test('should not accept null category', () => {
+    const category: ICategoryData = {
+      id: null,
+      name: null,
+      description: null,
+      created_at: null,
+    };
     expect(Category.validate(category)).toBeFalsy();
   });
 
-  test('should only accept 100 char', () => {
-    const category = 'c'.repeat(101);
+  test('should only accept name with 100 char', () => {
+    const category: ICategoryData = {
+      id: 'my id',
+      name: 'my name'.repeat(101),
+      description: 'my description',
+      created_at: new Date(),
+    };
     expect(Category.validate(category)).toBeFalsy();
   });
 
-  test('should only accept valid chars', () => {
-    const category = 'category;';
+  test('should only accept description with 200 char', () => {
+    const category: ICategoryData = {
+      id: 'my id',
+      name: 'my name',
+      description: 'my description'.repeat(101),
+      created_at: new Date(),
+    };
     expect(Category.validate(category)).toBeFalsy();
   });
 });
