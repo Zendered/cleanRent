6c3611e90c4431ba5caf3b187f8107a996157587
Luiz <luizz.ggr@gmail.com>
2022-02-01 14:19:26 -0300

    feat: validating if category has invalid data and if its null

--- src/entities/category.ts
+++ src/entities/category.ts
@@ -1,15 +1,14 @@
+import { ICategoryData } from '@/entities/';
+
 export class Category {
-  static validate(category:string):boolean {
-    if (!category) {
+  static validate({
+    id, name, description, created_at,
+  }:ICategoryData): boolean {
+    if (!id || !name || !description || !created_at) {
       return false;
     }
 
-    if (category.length > 100) {
-      return false;
-    }
-
-    const categoryRegex = /^[-!#$%&'+/0-9=?A-Z^_a-z`{|}~]+$/;
-    if (!categoryRegex.test(category)) {
+    if (name.length > 100 || description.length > 200) {
       return false;
     }
 
