0c1312082484cc7d15d3a0b13b650198cd78fc5f
Luiz <luizz.ggr@gmail.com>
2022-02-08 03:54:12 -0300

    test: validade category data

---
+++ test/entities/category.spec.ts
@@ -0,0 +1,27 @@
+import { Category } from '@/entities';
+import { InvalidNameError } from '@/entities/errors/';
+import { left } from '@/shared/';
+
+describe('Category entity', () => {
+  test('should not create category with invalid name', () => {
+    const category = {
+      id: 'my id',
+      name: 'my name'.repeat(101),
+      description: 'my description',
+      created_at: new Date(),
+    };
+    const error = Category.create(category);
+    expect(error).toEqual(left(new InvalidNameError()));
+  });
+
+  test('should not create Category with invalid description', () => {
+    const category = {
+      id: 'my id',
+      name: 'my name',
+      description: 'my description'.repeat(201),
+      created_at: new Date(),
+    };
+    const error = Category.create(category);
+    expect(error).toEqual(left(new InvalidNameError()));
+  });
+});
