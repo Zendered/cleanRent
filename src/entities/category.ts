77a89013179575c31f97a69210824a85489ba973
Luiz <luizz.ggr@gmail.com>
2022-02-08 18:39:36 -0300

    feat: create valid category

--- src/entities/category.ts
+++ src/entities/category.ts
@@ -1,13 +1,18 @@
-import { Either, left } from '@/shared/';
+import { Either, left, right } from '@/shared/';
 import { Name, ICategoryDTO } from '.';
 import { Description } from './category-description';
 import { InvalidNameError } from './errors';
 import { InvalidDescriptionError } from './errors/invalid-description-error';
 
 export class Category {
-  private constructor(private readonly name: Name, private readonly description:Description) {}
+  public readonly id: string;
+  public readonly created_at: Date;
+  private constructor(public readonly name: Name, public readonly description:Description) {
+    this.id = 'my id';
+    this.created_at = new Date();
+  }
   // eslint-disable-next-line consistent-return
-  static create(category:ICategoryDTO):Either<InvalidNameError | InvalidDescriptionError, Name > {
+  static create(category:ICategoryDTO):Either<InvalidNameError | InvalidDescriptionError, Category> {
     const nameOrError = Name.create(category.name);
     if (nameOrError.isLeft()) {
       return left(new InvalidNameError());
@@ -17,5 +22,10 @@ export class Category {
     if (descriptionOrError.isLeft()) {
       return left(new InvalidDescriptionError());
     }
+
+    const name: Name = nameOrError.value as Name;
+    const description: Description = descriptionOrError.value as Description;
+
+    return right(new Category(name, description));
   }
 }
