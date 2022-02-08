31d7cee48d553732b2a02c50d1db0dcd9cc27611
Luiz <luizz.ggr@gmail.com>
2022-02-08 03:49:00 -0300

    refactor: changing categoryData name

--- src/usecases/create-category/repository/inMemory-category-repository.ts
+++ src/usecases/create-category/repository/inMemory-category-repository.ts
@@ -1,30 +1,30 @@
 import { ICategoryRepository } from '@/usecases/ports/category-repository';
-import { ICategoryData } from '@/entities/category-data';
+import { ICategoryDTO } from '@/entities/category-dto';
 
 export class InMemoryCategoryRepository implements ICategoryRepository {
-  private repository:ICategoryData[];
+  private repository:ICategoryDTO[];
 
-  constructor(repository: ICategoryData[]) {
+  constructor(repository: ICategoryDTO[]) {
     this.repository = repository;
   }
 
-  async add(category: ICategoryData): Promise<void> {
+  async add(category: ICategoryDTO): Promise<void> {
     const exists = await this.exists(category);
     if (!exists) {
       this.repository.push(category);
     }
   }
 
-  async findCategoryById(id: string): Promise<ICategoryData> {
+  async findCategoryById(id: string): Promise<ICategoryDTO> {
     const category = await this.repository.find(category => category.id === id);
     return category || null;
   }
 
-  async findAllCategories(): Promise<ICategoryData[]> {
+  async findAllCategories(): Promise<ICategoryDTO[]> {
     return this.repository;
   }
 
-  async exists(category: ICategoryData): Promise<boolean> {
+  async exists(category: ICategoryDTO): Promise<boolean> {
     if (await this.findCategoryById(category.id) === null) {
       return false;
     }
