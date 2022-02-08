9f1832ec5167e6c52bc6e23a5756616bad088349
Luiz <luizz.ggr@gmail.com>
2022-02-08 17:29:55 -0300

    test: should not create category if name has less than 3 chars and description has less than 5 chars

--- test/entities/category.spec.ts
+++ test/entities/category.spec.ts
@@ -3,10 +3,10 @@ import { InvalidNameError, InvalidDescriptionError } from '@/entities/errors/';
 import { left } from '@/shared/';
 
 describe('Category entity', () => {
-  test('should not create category with invalid name', () => {
+  test('should not create category if name has more than 100 chars', () => {
     const category = {
       id: 'my id',
-      name: 'my name'.repeat(101),
+      name: 'N'.repeat(101),
       description: 'my description',
       created_at: new Date(),
     };
@@ -14,11 +14,33 @@ describe('Category entity', () => {
     expect(error).toEqual(left(new InvalidNameError()));
   });
 
-  test('should not create Category with invalid description', () => {
+  test('should not create Category if description has more than 225 chars', () => {
     const category = {
       id: 'my id',
       name: 'my name',
-      description: 'my description'.repeat(201),
+      description: 'D'.repeat(226),
+      created_at: new Date(),
+    };
+    const error = Category.create(category);
+    expect(error).toEqual(left(new InvalidDescriptionError()));
+  });
+
+  test('should not create Category if description has less then 5 chars', () => {
+    const category = {
+      id: 'my id',
+      name: 'my name',
+      description: 'D       ',
+      created_at: new Date(),
+    };
+    const error = Category.create(category);
+    expect(error).toEqual(left(new InvalidDescriptionError()));
+  });
+
+  test('should not create Category if name has less than 3 chars', () => {
+    const category = {
+      id: 'my id',
+      name: 'N      ',
+      description: 'my description',
       created_at: new Date(),
     };
     const error = Category.create(category);
