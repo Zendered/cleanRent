eb4ec8e6016d326117927c7064a63a341ac8c39d
Luiz <luizz.ggr@gmail.com>
2022-02-08 03:53:18 -0300

    test: validade name and description data

--- test/entities/category-validation.spec.ts
+++ test/entities/category-validation.spec.ts
@@ -1,34 +1,44 @@
-import { Category } from '@/entities/category';
-import { ICategoryData } from '@/entities/';
+import { Name } from '@/entities/category-name';
+import { ICategoryDTO } from '@/entities/';
 
 describe('Category validation', () => {
-  test('should not accept null category', () => {
-    const category: ICategoryData = {
+  test('should not accept null category description', () => {
+    const category: ICategoryDTO = {
       id: null,
       name: null,
       description: null,
       created_at: null,
     };
-    expect(Category.validate(category)).toBeFalsy();
+    expect(Name.validate(category.description)).toBeFalsy();
+  });
+
+  test('should not accept null category name', () => {
+    const category: ICategoryDTO = {
+      id: null,
+      name: null,
+      description: null,
+      created_at: null,
+    };
+    expect(Name.validate(category.name)).toBeFalsy();
   });
 
   test('should only accept name with 100 char', () => {
-    const category: ICategoryData = {
+    const category: ICategoryDTO = {
       id: 'my id',
       name: 'my name'.repeat(101),
       description: 'my description',
       created_at: new Date(),
     };
-    expect(Category.validate(category)).toBeFalsy();
+    expect(Name.validate(category.name)).toBeFalsy();
   });
 
-  test('should only accept description with 200 char', () => {
-    const category: ICategoryData = {
+  test('should only accept description with 225 char', () => {
+    const category: ICategoryDTO = {
       id: 'my id',
       name: 'my name',
-      description: 'my description'.repeat(101),
+      description: 'my description'.repeat(226),
       created_at: new Date(),
     };
-    expect(Category.validate(category)).toBeFalsy();
+    expect(Name.validate(category.description)).toBeFalsy();
   });
 });
