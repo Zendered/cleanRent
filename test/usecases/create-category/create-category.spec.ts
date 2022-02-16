1251a497052c672f683afeb8454316db38ada23e
Luiz <luizz.ggr@gmail.com>
2022-02-16 16:27:28 -0300

    test: Should not create category with invalid data

--- test/usecases/create-category/create-category.spec.ts
+++ test/usecases/create-category/create-category.spec.ts
@@ -2,6 +2,8 @@ import { ICategoryDTO } from '@/entities';
 import { ICategoryRepository } from '@/usecases/ports/category-repository';
 import { InMemoryCategoryRepository } from '@/usecases/create-category/repository/inMemory-category-repository';
 import { CreateCategory } from '@/usecases/create-category/create-category';
+import { left } from '@/shared';
+import { InvalidDescriptionError } from '@/entities/errors';
 
 describe('Create category use case', () => {
   const categories: ICategoryDTO[] = [];
@@ -17,5 +19,16 @@ describe('Create category use case', () => {
     const category = await repo.findCategoryById('my id');
     expect(category.name).toBe('my name');
     expect(response.value.name).toBe('my name');
+  });
+
+  test('Should not create category with invalid data', async () => {
+    const response = await usecase.perform({
+      id: 'invalid data',
+      name: 'my name',
+      description: 'm     ',
+    });
+    const category = await repo.findCategoryById('invalid data');
+    expect(category).toBeNull();
+    expect(response).toEqual(left(new InvalidDescriptionError()));
   });
 });
