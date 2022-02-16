2f82f6a09e33b15f1ab5de478a77570dafbdaace
Luiz <luizz.ggr@gmail.com>
2022-02-16 15:48:35 -0300

    refactor: removing public tag

--- src/entities/category.ts
+++ src/entities/category.ts
@@ -5,11 +5,16 @@ import { InvalidNameError } from './errors';
 import { InvalidDescriptionError } from './errors/invalid-description-error';
 
 export class Category {
-  public readonly id: string;
-  public readonly created_at: Date;
-  private constructor(public readonly name: Name, public readonly description:Description) {
-    this.id = 'my id';
-    this.created_at = new Date();
+  readonly id: string = 'my id';
+  readonly name: Name;
+  readonly description:Description;
+  readonly created_at: Date = new Date();
+
+  private constructor({
+    name, description,
+  }:{name:Name, description: Description}) {
+    this.name = name;
+    this.description = description;
   }
   // eslint-disable-next-line consistent-return
   static create(category:ICategoryDTO):Either<InvalidNameError | InvalidDescriptionError, Category> {
@@ -26,6 +31,6 @@ export class Category {
     const name: Name = nameOrError.value as Name;
     const description: Description = descriptionOrError.value as Description;
 
-    return right(new Category(name, description));
+    return right(new Category({ name, description }));
   }
 }
