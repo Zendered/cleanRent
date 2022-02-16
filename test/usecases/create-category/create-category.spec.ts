b51274448ba6504d3fbb69f305df8b0d8de572f1
Luiz <luizz.ggr@gmail.com>
2022-02-16 15:46:03 -0300

    test: register category

--- test/usecases/create-category/create-category.spec.ts
+++ test/usecases/create-category/create-category.spec.ts
@@ -1,18 +1,21 @@
 import { ICategoryDTO } from '@/entities';
+import { ICategoryRepository } from '@/usecases/ports/category-repository';
+import { InMemoryCategoryRepository } from '@/usecases/create-category/repository/inMemory-category-repository';
+import { CreateCategory } from '@/usecases/create-category/create-category';
 
 describe('Create category use case', () => {
   const categories: ICategoryDTO[] = [];
-  //   const repo: CategoryRepository = new CategoryRepositoryInMemory(categories);
-  //   const usecase: CreateCategory = new CreateCategory(repo);
-  test('should create a category', async () => {
-    console.log(categories);
+  const repo: ICategoryRepository = new InMemoryCategoryRepository(categories);
+  const usecase: CreateCategory = new CreateCategory(repo);
+  test('Should create a category with valid data', async () => {
+    const response = await usecase.perform({
+      id: 'my id',
+      name: 'my name',
+      description: 'my description',
+      created_at: new Date(),
+    });
+    const category = await repo.findCategoryById('my id');
+    expect(category.name).toBe('my name');
+    expect(response.value.name).toBe('my name');
   });
-//     const id = 'my id';
-//     const name = 'my name';
-//     const description = 'my description';
-//     // const created_at = new Date();
-//     const response = await usecase.createCategory({ name, description });
-//     const category = await repo.findCategoryById('meu id');
-//     expect(category.name).toBe('my name');
-//   });
 });
