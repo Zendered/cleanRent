4bd7cdfc3129f85d89012506984e51666c26739b
Luiz <luizz.ggr@gmail.com>
2022-02-16 15:59:20 -0300

    refactor: changing data to dto

--- src/usecases/ports/category-repository.ts
+++ src/usecases/ports/category-repository.ts
@@ -1,8 +1,8 @@
-import { ICategoryData } from '@/entities';
+import { ICategoryDTO } from '@/entities';
 
 export interface ICategoryRepository {
-add(category: ICategoryData): Promise<void>
-findCategoryById(id: string): Promise<ICategoryData>
-findAllCategories(): Promise<ICategoryData[]>
-exists(category: ICategoryData): Promise<boolean>
+add(category: ICategoryDTO): Promise<void>
+findCategoryById(id: string): Promise<ICategoryDTO>
+findAllCategories(): Promise<ICategoryDTO[]>
+exists(category: ICategoryDTO): Promise<boolean>
 }
