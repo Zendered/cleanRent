2d7a405ee5ff8b9cc447e412701e1e98fb5c89cd
Luiz <luizz.ggr@gmail.com>
2022-02-17 19:00:39 -0300

    test: web controller should return 400 when category has invalid name or description

--- test/web-controtrollers/create-category-controller.spec.ts
+++ test/web-controtrollers/create-category-controller.spec.ts
@@ -1,4 +1,5 @@
 import { ICategoryDTO } from '@/entities';
+import { InvalidDescriptionError, InvalidNameError } from '@/entities/errors';
 import { CreateCategory } from '@/usecases/create-category/create-category';
 import { InMemoryCategoryRepository } from '@/usecases/create-category/repository/inMemory-category-repository';
 import { ICategoryRepository } from '@/usecases/ports/category-repository';
@@ -24,18 +25,33 @@ describe('Create category web controller', () => {
     expect(response.body).toEqual(request.body);
   });
 
-  test('Should return 201 when request contains valid category data', async () => {
-    const request: IHttpRequest = {
+  test('Should return 400 when request contains invalid name', async () => {
+    const requestWithInvalidName: IHttpRequest = {
       body: {
         id: 'my id',
-        name: 'my name',
+        name: 'N      ',
         description: 'my description',
         created_at: new Date(),
       },
     };
     const controller: CreateCategoryController = new CreateCategoryController(usecase);
-    const response: IHttpResponse = await controller.handle(request);
-    expect(response.statusCode).toEqual(201);
-    expect(response.body).toEqual(request.body);
+    const response: IHttpResponse = await controller.handle(requestWithInvalidName);
+    expect(response.statusCode).toEqual(400);
+    expect(response.body).toBeInstanceOf(InvalidNameError);
+  });
+
+  test('Should return 400 when request contains invalid description', async () => {
+    const requestWithInvalidDescription: IHttpRequest = {
+      body: {
+        id: 'my id',
+        name: 'my name',
+        description: 'D     ',
+        created_at: new Date(),
+      },
+    };
+    const controller: CreateCategoryController = new CreateCategoryController(usecase);
+    const response: IHttpResponse = await controller.handle(requestWithInvalidDescription);
+    expect(response.statusCode).toEqual(400);
+    expect(response.body).toBeInstanceOf(InvalidDescriptionError);
   });
 });
