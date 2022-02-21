b932c1764319be8508d6d3054bcccca71feccbd2
Luiz <luizz.ggr@gmail.com>
2022-02-21 14:53:37 -0300

    refactor: move middleware and routes folder inside main folter

--- src/main/middleware/content-type.ts
+++ src/main/middleware/content-type.ts
@@ -1,5 +1,4 @@
 import { Request, Response, NextFunction } from 'express';
-import app from '../app';
 
 export const contentType = (req:Request, res:Response, next:NextFunction): void => {
   res.type('json');
