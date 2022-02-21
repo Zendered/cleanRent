8676dee823cad97fab82382b5e37ed2f35043852
Luiz <luizz.ggr@gmail.com>
2022-02-21 13:30:17 -0300

    feat: express route adapter

---
+++ src/main/config/adapters/express-route-adapter.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import { CreateCategoryController } from '@/web-controllers/';
+import { IHttpRequest } from '@/web-controllers/ports';
+
+export const adaptRout = (controller: CreateCategoryController) => async (req:Request, res:Response) => {
+  const httpRequest: IHttpRequest = {
+    body: req.body,
+  };
+  const httpResponse = await controller.handle(httpRequest);
+  res.status(httpResponse.statusCode).json(httpResponse.body);
+};
