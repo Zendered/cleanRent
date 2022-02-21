099e056325dc36bec8a7aba4a2396ebd48c09608
Luiz <luizz.ggr@gmail.com>
2022-02-21 14:20:11 -0300

    test: config register route

---
+++ test/main/config/routes/register-route.test.ts
@@ -0,0 +1,19 @@
+import request from 'supertest';
+import app from '@/main/config/app';
+
+describe('Register route', () => {
+  test('should return an category on success', async () => {
+    app.post('/test_cors', (req, res) => {
+      res.send();
+    });
+    await request(app)
+      .post('/api/register')
+      .send({
+        id: 'my id',
+        name: 'my name',
+        description: 'my description',
+        created_at: new Date(),
+      })
+      .expect(201);
+  });
+});
