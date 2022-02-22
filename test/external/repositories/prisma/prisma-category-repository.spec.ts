a1b3114269f13c9d1d066fdc34255e67ffa115aa
Luiz <luizz.ggr@gmail.com>
2022-02-22 17:44:08 -0300

    test: find all existing categories

--- test/external/repositories/prisma/prisma-category-repository.spec.ts
+++ test/external/repositories/prisma/prisma-category-repository.spec.ts
@@ -21,4 +21,23 @@ describe('Prisma Category repository', () => {
     };
     expect(categoryRepository.exists(category)).toBeTruthy();
   });
+
+  test('should find all existing categories ', async () => {
+    const categoryRepository = new PrismaCategoryRepository(prisma);
+    await categoryRepository.add({
+      name: 'my name01',
+      description: 'my description01',
+    });
+
+    await categoryRepository.add({
+      name: 'my name02',
+      description: 'my description02',
+    });
+
+    const result = await categoryRepository.findAllCategories();
+    expect(result[0].name).toBe('my name01');
+    expect(result[0].description).toBe('my description01');
+    expect(result[1].name).toBe('my name02');
+    expect(result[1].description).toBe('my description02');
+  });
 });
