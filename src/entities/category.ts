946aa7b313bee8b0164cf032310ad9009ec4d47a
Luiz <luizz.ggr@gmail.com>
2022-02-22 16:59:11 -0300

    refactor: adding id and created_at to category contructor

--- src/entities/category.ts
+++ src/entities/category.ts
@@ -5,16 +5,18 @@ import { InvalidNameError } from './errors';
 import { InvalidDescriptionError } from './errors/invalid-description-error';
 
 export class Category {
-  readonly id: string = 'my id';
+  readonly id: string;
   readonly name: Name;
   readonly description:Description;
-  readonly created_at: Date = new Date();
+  readonly created_at: Date;
 
   private constructor({
-    name, description,
-  }:{name:Name, description: Description}) {
+    id, name, description, created_at,
+  }:{ id: string, name:Name, description: Description, created_at: Date}) {
+    this.id = id ?? 'my id';
     this.name = name;
     this.description = description;
+    this.created_at = created_at ?? new Date();
   }
   // eslint-disable-next-line consistent-return
   static create(category:ICategoryDTO):Either<InvalidNameError | InvalidDescriptionError, Category> {
@@ -28,9 +30,13 @@ export class Category {
       return left(new InvalidDescriptionError(category.description));
     }
 
+    const id: string = 'my id';
     const name: Name = nameOrError.value as Name;
     const description: Description = descriptionOrError.value as Description;
+    const created_at = new Date();
 
-    return right(new Category({ name, description }));
+    return right(new Category({
+      id, name, description, created_at,
+    }));
   }
 }
